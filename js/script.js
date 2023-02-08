801e018c3b602bb92e64bc5fa453787f8461a900
Ilya Tupalski <itupalski@gmail.com>
2023-02-08 15:04:10 +0400

    Modal-Modifications

--- js/script.js
+++ js/script.js
@@ -96,13 +96,16 @@ window.addEventListener('DOMContentLoaded', () =>{
               modal = document.querySelector('.modal'),
               modalCloseBtn = document.querySelector('[data-close]');
 
-        modalTrigger.forEach(btn => {
-          btn.addEventListener('click', () => {
-            modal.style.display = 'block';
-            document.body.style.overflow = 'hidden';
-          });
-        });     
+        function openModal(){
+          modal.style.display = 'block';
+          document.body.style.overflow = 'hidden';
+          clearInterval(modalTimerId);
+        }      
 
+        modalTrigger.forEach(btn => {
+          btn.addEventListener('click', openModal);
+        });  
+        
         function closeModal(){
           modal.style.display = 'none';
           document.body.style.overflow = '';
@@ -121,4 +124,15 @@ window.addEventListener('DOMContentLoaded', () =>{
             closeModal();
           }
         });
+
+        const modalTimerId = setTimeout(openModal, 5000);
+
+        function showModalByScroll() {
+          if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+          }
+        }
+
+        window.addEventListener('scroll', showModalByScroll);
 });
