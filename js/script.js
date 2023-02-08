15387af18f85d57d184fdf926b999f0f854bc685
Ilya Tupalski <itupalski@gmail.com>
2023-02-08 14:50:04 +0400

    Modal-Window

--- js/script.js
+++ js/script.js
@@ -89,4 +89,36 @@ window.addEventListener('DOMContentLoaded', () =>{
         }
 
         setClock('.timer', deadLine);
+
+        // modal
+
+        const modalTrigger = document.querySelectorAll('[data-modal]'),
+              modal = document.querySelector('.modal'),
+              modalCloseBtn = document.querySelector('[data-close]');
+
+        modalTrigger.forEach(btn => {
+          btn.addEventListener('click', () => {
+            modal.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+          });
+        });     
+
+        function closeModal(){
+          modal.style.display = 'none';
+          document.body.style.overflow = '';
+        }
+
+        modalCloseBtn.addEventListener('click', closeModal);
+
+        modal.addEventListener('click', (e) => {
+          if (e.target === modal) {
+            closeModal();
+          }
+        });
+
+        document.addEventListener('keydown', (e) => {
+          if (e.code === "Escape" && modal.style.display == 'block') {
+            closeModal();
+          }
+        });
 });
