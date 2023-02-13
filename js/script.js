9822337d273a1ef5fafd450d6002fcb7c5053d40
Ilya Tupalski <itupalski@gmail.com>
2023-02-13 12:42:08 +0400

    class-card

--- js/script.js
+++ js/script.js
@@ -125,7 +125,7 @@ window.addEventListener('DOMContentLoaded', () =>{
           }
         });
 
-        const modalTimerId = setTimeout(openModal, 5000);
+        // const modalTimerId = setTimeout(openModal, 5000);
 
         function showModalByScroll() {
           if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -135,4 +135,67 @@ window.addEventListener('DOMContentLoaded', () =>{
         }
 
         window.addEventListener('scroll', showModalByScroll);
+
+        // Используем классы для карточек 
+
+        class MenuCard {
+          constructor(src, alt, title, desrc, price, parentSelector){
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.desrc = desrc;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAN();
+          }
+
+          changeToUAN() {
+            this.price = this.price * this.transfer;
+          }
+
+          render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+            <div class="menu__item">
+              <img src=${this.src} alt=${this.alt}>
+              <h3 class="menu__item-subtitle">${this.title}</h3>
+              <div class="menu__item-descr">${this.desrc}</div>
+                <div class="menu__item-divider"></div>
+                  <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                  <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+              </div>
+            </div>
+            `;
+            this.parent.append(element);
+          }
+        }
+
+        new MenuCard(
+          "img/tabs/vegy.jpg",
+          "vegy",
+          'Меню "Фитнес"',
+          'Меню "Фитнес" - это новый подход к приготовлению блюд',
+          9,
+          '.menu .container'
+        ).render();
+
+        new MenuCard(
+          "img/tabs/vegy.jpg",
+          "vegy",
+          'Меню "Фитнес"',
+          'Меню "Фитнес" - это новый подход к приготовлению блюд',
+          9,
+          '.menu .container'
+        ).render();
+
+        new MenuCard(
+          "img/tabs/vegy.jpg",
+          "vegy",
+          'Меню "Фитнес"',
+          'Меню "Фитнес" - это новый подход к приготовлению блюд',
+          9,
+          '.menu .container'
+        ).render();
 });
