c36466c1c7f66bef1aee33347cf7133da0b036f7
Ilya Tupalski <itupalski@gmail.com>
2023-02-13 13:10:03 +0400

    Rest-operator

--- js/script.js
+++ js/script.js
@@ -125,7 +125,7 @@ window.addEventListener('DOMContentLoaded', () =>{
           }
         });
 
-        // const modalTimerId = setTimeout(openModal, 5000);
+        const modalTimerId = setTimeout(openModal, 5000);
 
         function showModalByScroll() {
           if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -139,12 +139,13 @@ window.addEventListener('DOMContentLoaded', () =>{
         // Используем классы для карточек 
 
         class MenuCard {
-          constructor(src, alt, title, desrc, price, parentSelector){
+          constructor(src, alt, title, desrc, price, parentSelector, ...classes){
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.desrc = desrc;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.changeToUAN();
@@ -156,8 +157,15 @@ window.addEventListener('DOMContentLoaded', () =>{
 
           render() {
             const element = document.createElement('div');
+
+            if(this.classes.length === 0){
+              this.element = 'menu__item';
+              element.classList.add(this.element);
+            } else {
+              this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `
-            <div class="menu__item">
               <img src=${this.src} alt=${this.alt}>
               <h3 class="menu__item-subtitle">${this.title}</h3>
               <div class="menu__item-descr">${this.desrc}</div>
@@ -166,7 +174,6 @@ window.addEventListener('DOMContentLoaded', () =>{
                     <div class="menu__item-cost">Цена:</div>
                   <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
               </div>
-            </div>
             `;
             this.parent.append(element);
           }
@@ -178,7 +185,7 @@ window.addEventListener('DOMContentLoaded', () =>{
           'Меню "Фитнес"',
           'Меню "Фитнес" - это новый подход к приготовлению блюд',
           9,
-          '.menu .container'
+          '.menu .container',
         ).render();
 
         new MenuCard(
@@ -187,7 +194,9 @@ window.addEventListener('DOMContentLoaded', () =>{
           'Меню "Фитнес"',
           'Меню "Фитнес" - это новый подход к приготовлению блюд',
           9,
-          '.menu .container'
+          '.menu .container',
+          'menu__item',
+          'big'
         ).render();
 
         new MenuCard(
@@ -196,6 +205,8 @@ window.addEventListener('DOMContentLoaded', () =>{
           'Меню "Фитнес"',
           'Меню "Фитнес" - это новый подход к приготовлению блюд',
           9,
-          '.menu .container'
+          '.menu .container',
+          'menu__item',
+          'big'
         ).render();
 });
